146d4932f881b04f8a1991ebfff314b536db91e1
Lilo Heinrich <liloheinrich@Lilos-MacBook-Pro.local>
2019-01-18 00:04:53 -0800

    added network table accessors for vision

--- src/main/java/org/usfirst/frc/team2557/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team2557/robot/Robot.java
@@ -2,6 +2,10 @@ package org.usfirst.frc.team2557.robot;
 
 import org.usfirst.frc.team2557.robot.subsystems.GyroSwerveDrive;
 import org.usfirst.frc.team2557.robot.subsystems.SwerveDrive;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -47,10 +51,10 @@ public class Robot extends TimedRobot {
 		m_autonomousCommand = m_chooser.getSelected();
 
 		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
+		 * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+		 * switch(autoSelected) { case "My Auto": autonomousCommand = new
+		 * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+		 * ExampleCommand(); break; }
 		 */
 
 		// schedule the autonomous command (example)
@@ -62,6 +66,39 @@ public class Robot extends TimedRobot {
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+
+		NetworkTable contoursTable = NetworkTableInstance.getDefault().getTable("/vision/contours");
+		NetworkTableEntry centerXs = contoursTable.getEntry("centerX");
+		NetworkTableEntry centerYs = contoursTable.getEntry("centerY");
+		NetworkTableEntry heights = contoursTable.getEntry("height");
+		NetworkTableEntry widths = contoursTable.getEntry("width");
+		NetworkTableEntry areas = contoursTable.getEntry("area");
+		NetworkTableEntry distances = contoursTable.getEntry("distance");
+		NetworkTableEntry rotations = contoursTable.getEntry("rotation");
+
+		double[] xs = new double[0];
+		double[] ys = new double[0];
+		double[] hs = new double[0];
+		double[] ws = new double[0];
+		double[] as = new double[0];
+		double[] ds = new double[0];
+		double[] rs = new double[0];
+
+		centerXs.getDoubleArray(xs);
+		centerYs.getDoubleArray(ys);
+		heights.getDoubleArray(hs);
+		widths.getDoubleArray(ws);
+		areas.getDoubleArray(as);
+		distances.getDoubleArray(ds);
+		rotations.getDoubleArray(rs);
+
+		System.out.println("xs: " + xs.toString());
+		System.out.println("ys: " + ys.toString());
+		System.out.println("hs: " + hs.toString());
+		System.out.println("ws: " + ws.toString());
+		System.out.println("as: " + as.toString());
+		System.out.println("ds: " + ds.toString());
+		System.out.println("rs: " + rs.toString());
 	}
 
 	@Override
